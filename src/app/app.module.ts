33e32140fdc62172378aa95806063ca7c88ea45c
Okashoon <okashoon@gmail.com>
2017-03-04 08:12:07 +0200

    adding grocery html and input component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { RouterModule, Routes } from '@angular/router';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
@@ -8,6 +9,13 @@ import { HeaderComponent } from './header/header.component';
 import { GroceryCalcComponent } from './grocery-calc/grocery-calc.component';
 import { HomeloanCalcComponent } from './homeloan-calc/homeloan-calc.component';
 import { BusinessCalcComponent } from './business-calc/business-calc.component';
+import { DropdownDirective } from './dropdown.directive';
+import { InputGroupComponent } from './input-group/input-group.component';
+import { ResultsComponent } from './results/results.component';
+
+const appRoutes: Routes = [
+  {path:'grocery', component: GroceryCalcComponent}
+] 
 
 @NgModule({
   declarations: [
@@ -15,9 +23,13 @@ import { BusinessCalcComponent } from './business-calc/business-calc.component';
     HeaderComponent,
     GroceryCalcComponent,
     HomeloanCalcComponent,
-    BusinessCalcComponent
+    BusinessCalcComponent,
+    DropdownDirective,
+    InputGroupComponent,
+    ResultsComponent
   ],
   imports: [
+    RouterModule.forRoot(appRoutes),
     BrowserModule,
     FormsModule,
     HttpModule
---
+++ src/app/dropdown.directive.ts
@@ -0,0 +1,23 @@
+import { Directive, ElementRef, HostListener, HostBinding } from '@angular/core';
+
+@Directive({
+  selector: '[appDropdown]'
+})
+export class DropdownDirective {
+
+  opened = false;
+
+  constructor(private elementRef: ElementRef) { }
+
+@HostListener('click') onClick(){
+  this.opened = !this.opened;
+}
+@HostListener('mouseleave') onLeave(){
+  this.opened = false;
+}
+@HostBinding('class.open') get onOpen(){
+  return this.opened
+}
+
+
+}
